dc0a680fb5a478a28ac87701718915fdd155ac92
fardeenpathan <md.fardeen5236@gmail.com>
2026-08-20 14:54:46 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -265,10 +265,10 @@ export default function Home() {
         <div className=" md:pt-24 pt-4 md:pb-50 lg:pb-160 relative z-10 pb-24">
           <h2 className="text-[#15528A]  text-center leading-tight text-[30px] sm:text-5xl lg:text-[100px] md:pb-370 pb-0 flex flex-col items-center gap-y-2">
            <span className="font-parisienne [text-shadow:2px_2px_4px_rgba(0,0,0,0.4)]">
-  Tejas Patel
+  Tejas
 </span>
             <span className="text-xl sm:text-3xl tracking-[10px] font-cormorant [text-shadow:2px_2px_4px_rgba(0,0,0,0.4)]">WEDS</span>
-            <span className="font-parisienne [text-shadow:2px_2px_4px_rgba(0,0,0,0.4)]">Nikita Patel</span>
+            <span className="font-parisienne [text-shadow:2px_2px_4px_rgba(0,0,0,0.4)]">Nikita</span>
           </h2>
 
           <div className="flex flex-col items-center text-center gap-6 mt-0  lg:pt-200 pt-150 3xl:pt-280">
@@ -307,7 +307,7 @@ export default function Home() {
 
             <h2 className="text-white font-Cormorant-upright text-center mt-14
             md:text-5xl text-[64px] lg:text-[100px] leading-tight font-bold">
-               Tejas Patel
+               Tejas
             </h2>
 
 
@@ -322,7 +322,7 @@ export default function Home() {
             text-[64px] sm:text-7xl lg:text-[100px] leading-tight font-bold">
               <span className="text-white font-Cormorant-upright text-center lg:mt-10 mt-4 
             md:text-5xl text-[82px] lg:text-[150px] leading-tight">&</span>   <br />
-              Nikita Patel
+              Nikita
             </h2>
 
             <p className="text-white font-Cormorant-upright lg:text-[30px] md:text-2xl text-[16px] mt-2">
